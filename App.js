cffc7d92ed06a797f9cd6a88f7a5b5bf4e1e0566
vicky jaiswal <vickyjaiswal647@gmail.com>
2023-08-13 22:56:30 +0530

    created Food Villa app

--- App.js
+++ App.js
@@ -1,11 +1,663 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/**
+ * HMR - Hot Module Replacement, it has File Watcher Algorithm(written in c++)
+ * which tracks the changes in real time and update over browser synchronously
+ *
+ * dist folder keeps the files in minified forms
+ *
+ * (npx parcel build index.html) build the file and upload it over server in minified form
+ *
+ * We should put parcel-cache in the .gitignore file.
+ */
 
-const heading1 = React.createElement("h1", {}, "Hello Parcel");
-const heading2 = React.createElement("h2", {}, "Hello Vicky");
-const container = React.createElement("div", {}, [heading1, heading2]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+/**
+            Header
+                - Logo
+                - Nav Items(Right Side)
+                - Cart
+            Body
+                - Search bar
+                - RestaurantCard
+                    - Image
+                    - Name
+                    - Rating
+                    - Cusines
+            Footer
+                - Links
+                - Copyrights
+        */
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://yt3.googleusercontent.com/HipIhegQlrlSzBGZSUtOPYyA2VGbo2qyXmIdYNkodW-HVF01t5CX-MrXoDKnf9R5UErOmCbnbA=s900-c-k-c0x00ffffff-no-rj"
+    />
+  </a>
+);
+//component
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
+const data = [
+    {
+        id: '1',
+        imageUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xqepd6rivebgewjgi3sj",
+        name: "Domino's Pizza",
+        cusines: ['Pizzas', 'Italian', 'Desserts'],
+        rating: "4.2 stars"
+    },
+    {
+        id: '2',
+        imageUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ee5f8e06b300efc07c9fe3f4df40dfc4",
+        name: "McDonald's",
+        cusines: ['Burgers', 'American'],
+        rating: "4.3 stars"
+    },
+    {
+        id: '3',
+        imageUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/56c9ab92bd79745fd152a30fa2525426",
+        name: "KFC",
+        cusines: ['Italian'],
+        rating: "4.4 stars"
+    }
+];
+
+const restaurantList = [
+    {
+      "info": {
+        "id": "24439",
+        "name": "Domino's Pizza",
+        "cloudinaryImageId": "xqepd6rivebgewjgi3sj",
+        "locality": "Shoppers Orbit",
+        "areaName": "Vishrantwadi",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Pizzas",
+          "Italian",
+          "Pastas",
+          "Desserts"
+        ],
+        "avgRating": 4.1,
+        "feeDetails": {
+          "restaurantId": "24439",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3000
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3000
+        },
+        "parentId": "2456",
+        "avgRatingString": "4.1",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 25,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25 mins",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-08-13 22:59:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹100 OFF",
+          "subHeader": "ABOVE ₹199",
+          "discountTag": "FLAT DEAL"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      }
+    },
+    {
+      "info": {
+        "id": "340851",
+        "name": "Pizza Hut",
+        "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+        "locality": "Dhanori Road",
+        "areaName": "Vishrantwadi",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Pizzas"
+        ],
+        "avgRating": 3.8,
+        "feeDetails": {
+          "restaurantId": "340851",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3000
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3000
+        },
+        "parentId": "721",
+        "avgRatingString": "3.8",
+        "totalRatingsString": "1K+",
+        "sla": {
+          "deliveryTime": 22,
+          "lastMileTravel": 1.1,
+          "serviceability": "SERVICEABLE",
+          "slaString": "22 mins",
+          "lastMileTravelString": "1.1 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-08-14 02:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹100 OFF",
+          "subHeader": "ABOVE ₹199",
+          "discountTag": "FLAT DEAL",
+          "discountCalloutInfo": {
+            "message": "Free Delivery",
+            "logoCtx": {
+              "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+            }
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      }     
+    },
+    {
+      "info": {
+        "id": "623603",
+        "name": "EatFit",
+        "cloudinaryImageId": "8a9029f512e2a7a37a8ed381322cec30",
+        "locality": "2nd Phase",
+        "areaName": "Dhanori",
+        "costForTwo": "₹270 for two",
+        "cuisines": [
+          "Chinese",
+          "Healthy Food",
+          "Tandoor",
+          "Pizzas",
+          "North Indian",
+          "Thalis",
+          "Biryani"
+        ],
+        "avgRating": 4,
+        "feeDetails": {
+          "restaurantId": "623603",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 4200
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 4200
+        },
+        "parentId": "76139",
+        "avgRatingString": "4.0",
+        "totalRatingsString": "500+",
+        "sla": {
+          "deliveryTime": 25,
+          "lastMileTravel": 4.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25 mins",
+          "lastMileTravelString": "4.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-08-14 01:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹125 OFF",
+          "subHeader": "ABOVE ₹249",
+          "discountTag": "FLAT DEAL",
+          "discountCalloutInfo": {
+            "message": "Free Delivery",
+            "logoCtx": {
+              "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+            }
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      }
+    },
+    {
+      "info": {
+        "id": "725894",
+        "name": "Big Bowl",
+        "cloudinaryImageId": "546730f50d0c2b6e9a1446022a08ed21",
+        "locality": "Kasturbawadi",
+        "areaName": "Vishrantwadi",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "North Indian",
+          "Chinese",
+          "Tibetan",
+          "Desserts"
+        ],
+        "avgRating": 3.8,
+        "feeDetails": {
+          "restaurantId": "725894",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3000
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3000
+        },
+        "parentId": "434792",
+        "avgRatingString": "3.8",
+        "totalRatingsString": "50+",
+        "sla": {
+          "deliveryTime": 16,
+          "lastMileTravel": 0.7,
+          "serviceability": "SERVICEABLE",
+          "slaString": "16 mins",
+          "lastMileTravelString": "0.7 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-08-13 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹100 OFF",
+          "subHeader": "ABOVE ₹199",
+          "discountTag": "FLAT DEAL",
+          "discountCalloutInfo": {
+            "message": "Free Delivery",
+            "logoCtx": {
+              "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+            }
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "isNewlyOnboarded": true,
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      }
+    },
+    {
+      "info": {
+        "id": "10485",
+        "name": "R K Pure Veg",
+        "cloudinaryImageId": "ahv5f382qzwzzzjgdzxq",
+        "locality": "Tingre Nagar",
+        "areaName": "Vishrantwadi",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "Mughlai",
+          "North Indian",
+          "Chinese"
+        ],
+        "avgRating": 3.9,
+        "veg": true,
+        "feeDetails": {
+          "restaurantId": "10485",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3000
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3000
+        },
+        "parentId": "163705",
+        "avgRatingString": "3.9",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 17,
+          "lastMileTravel": 0.8,
+          "serviceability": "SERVICEABLE",
+          "slaString": "17 mins",
+          "lastMileTravelString": "0.8 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-08-13 23:30:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹125 OFF",
+          "subHeader": "ABOVE ₹249",
+          "discountTag": "FLAT DEAL",
+          "discountCalloutInfo": {
+            "message": "Free Delivery",
+            "logoCtx": {
+              "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+            }
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      }
+    }
+];
+
+const RestaurantCard = (props) => {
+  return (
+    <div className="card" >
+      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" 
+        + props.cloudinaryImageId} />
+      <h2>{props.name}</h2>
+      <h3>{props.cuisines.join(",")}</h3>
+      <h3>{props.areaName}</h3>
+      <h4>{props.avgRating} stars</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+        {restaurantList.map((item)=>{
+            return <RestaurantCard {...item.info} key={...item.info.id} />
+        })}    
+    </div>
+    
+  )
+}
+
+const Footer = () => {
+  return <h3>Footer</h3>;
+};
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
 //ReactDOM
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
- 
+root.render(<AppLayout />);
